aa7b2bf3963247ffd19bee33975d20939e0b9249
Daniel <tyriar@tyriar.com>
2024-12-01 15:15:19 +0100

    ui

--- FrameUI/PlaneUi/examples/components/php/post.php
+++
@@ -1,7 +0,0 @@
-<?php
-      header("Cache-Control: no-cache, must-revalidate");
-      header("Pragma: no-cache");
-      header("Content-Type:text/html;charset=utf-8");
-    print_r($_POST);
-?>
-<meta charset="utf-8" />
--- FrameUI/PlaneUi/examples/components/php/rating.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	header("Access-Control-Allow-Origin: *");
-
-	if(isset($_POST)) {
-		echo number_format(($_POST['score'] + $_POST['newscore']) / 2, 2);
-	}
